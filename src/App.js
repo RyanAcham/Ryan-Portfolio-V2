4429fb8fc726e4f83a496216d6e9816e2e1cd1db
Ryan Acham <ryanacham@hotmail.com>
2025-01-03 23:13:34 -0500

    about me section and links

--- src/App.js
+++ src/App.js
@@ -16,28 +16,28 @@ export default function App() {
       url: "https://ryanacham.github.io/Frontend-Developer-Challenge-3-Dashboard-and-Project-Creation/",
       github: "https://github.com/RyanAcham/Frontend-Developer-Challenge-3-Dashboard-and-Project-Creation?tab=readme-ov-file",
       tech: "Javascript, CSS, HTML, React, GitHub",
-      imageUrl: "https://via.placeholder.com/150/FF0000",
+      imageUrl: "/img/project/prepr.png",
     },
     "Star Wars Database": {
-      description: "As the CSS/Tailwind developer for our Star Wars Database project, I was instrumental in creating and putting into use a responsive and aesthetically pleasing user interface. Utilizing a variety of APIs, the website—which was constructed with JavaScript, HTML, CSS, React, and jQuery—offers dynamic access to information about individuals, species, and planets from the Star Wars universe. In a matter of days, I became proficient with Tailwind, utilizing its utility-first architecture to effectively style elements and smoothly integrate functions such as lightmode and darkmode. This project was centered around teamwork; I worked closely with the team to support the integration of a dynamic search system, deploy the site on GitHub Pages, and ensure consistent styling. ",
+      description: "I was the CSS/Tailwind developer for our Star Wars Database project, which made me in charge of creating and putting into use a responsive and aesthetically pleasing user interface. Utilizing a variety of APIs, the website—which was constructed with JavaScript, HTML, CSS, React, and jQuery—offers dynamic access to information about individuals, species, and planets from the Star Wars universe. In a matter of days, I became proficient with Tailwind, utilizing its utility-first architecture to effectively style elements and smoothly integrate functions such as lightmode and darkmode. This project was centered around teamwork; I worked closely with the team to support the integration of a dynamic search system, deploy the site on GitHub Pages, and ensure consistent styling. ",
       url: "https://suedepritch.github.io/crispy-spork/",
       github: "https://github.com/SuedePritch/crispy-spork?tab=readme-ov-file",
       tech: "Javascript, HTML, CSS, React, Tailwind, jQuery, Various APIs, GitHub",
-      imageUrl: "https://via.placeholder.com/150/00FF00",
+      imageUrl: "/img/project/starwar.png",
     },
     "Weather Report": {
       description: "In this sideproject, I used HTML, CSS, and JavaScript to create a basic weather application with a responsive and slick user interface. Depending on the city typed into the search bar, the app displays important metrics like temperature, humidity, wind speed, and feels-like temperature by integrating with a weather API to retrieve real-time weather data. This project is a clear illustration of how I can combine clean front-end development techniques with dynamic APIs to create useful, user-friendly applications. ",
       url: "https://ryanacham.github.io/Weather-Report/",
       github: "https://github.com/RyanAcham/Weather-Report/tree/main",
       tech: "Javascript, CSS, HTML, Weather API, GitHub",
-      imageUrl: "https://via.placeholder.com/150/0000FF",
+      imageUrl: "/img/project/weather.png",
     },
     "Coding Quiz": {
       description: "This website is an interactive coding test that uses HTML, CSS, and JavaScript to provide an enjoyable user experience. To add challenge, the quiz has a countdown timer that subtracts time for each wrong response. The logic for monitoring, adjusting the timer, and controlling the user's progress through the questions is handled by JavaScript. CSS is used to style the front-end for a responsive and clean that makes the quiz look great on all devices. The website also uses local storage to keep scores between sessions and saves high scores so users can compete with others. Through interactive features, this project keeps users interested while providing a real-world application of front-end and back-end skills.",
       url: "https://ryanacham.github.io/Coding-Quiz-Mark-II/",
       github: "https://github.com/RyanAcham/Coding-Quiz-Mark-II",
       tech: "Javascript, CSS, HTML, GitHub",
-      imageUrl: "https://via.placeholder.com/150/FFFFFF",
+      imageUrl: "/img/project/quiz.png",
     }
   };
 
@@ -197,14 +197,14 @@ export default function App() {
       </div>
       <section>
         <div className="flex flex-col items-center gap-4">
-          <h3 className="text-6xl font-bold text-black dark:text-white pt-4">Portfolio</h3>
+          <h3 className="text-6xl font-bold text-black dark:text-white py-10">Portfolio</h3>
 
           {/* Portfolio Buttons */}
           <ul className="flex flex-wrap justify-center items-center gap-4">
             {Object.entries(websiteInfo).map(([website, info]) => (
               <li key={website} className="flex flex-col items-center">
                 <button onClick={() => toggleInfoModal(website)} className="text-center">
-                  <img src={info.imageUrl} alt={website} className="mb-2" />
+                  <img src={info.imageUrl} alt={website} style={{ height: '200px', objectFit: 'cover' }} className="mb-2" />
                   <span className="text-lg text-black dark:text-white">{website}</span>
                 </button>
               </li>
@@ -261,6 +261,67 @@ export default function App() {
             </div>
           </div>
         )}
+      </section>
+      <section class="flex flex-col justify-center items-center mt-20">
+        <h4 className="text-6xl font-bold text-black dark:text-white py-10">About Me</h4>
+          <div className="flex justify-center items-center p-10">
+            <div className="w-1/2">
+              <img 
+                src="me.1ebeee47e28fcec6e965.jpg" 
+                alt="Example Image" 
+                className="max-w-[600px] h-auto object-cover" 
+              />
+            </div>
+            <div className="w-1/3">
+              <p className="text-lg text-black dark:text-white">
+              Hi! My name is Ryan Acham, and I am a Full-Stack Developer with a solid foundation in web development. I graduated from the Full Stack Flex Program at the University of Toronto, where I developed my skills in HTML, CSS, JavaScript, and well-known frameworks like Tailwind, Node.js, Express.js, and React. Building dynamic, user-friendly web applications is something I have a keen interest in, and I thrive in both front-end and back-end development.
+              </p>
+              <br/>
+              <p className="text-lg text-black dark:text-white">
+                I worked on a number of projects during the program that gave me the chance to use my abilities in practical settings, such as creating server-side solutions and interactive user interfaces. As I begin my career in full-stack development, I am eager to contribute to creative projects and am quick to pick up new technologies and approaches.
+              </p>
+            </div>
+          </div>
+      </section>
+      <section class="flex flex-wrap justify-center items-center gap-4">
+        <div className="flex flex-wrap justify-center items-center gap-4">
+          <div className="flex justify-center items-center gap-8 p-10">
+            <a
+              href="https://github.com/RyanAcham"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative w-16 h-16  rounded-lg overflow-hidden shadow-lg hover:scale-105 transform transition-all duration-300"
+            >
+              <img
+                src="githublm.png"
+                alt="github"
+                className="absolute top-0 left-0 w-full h-full object-cover dark:hidden"
+              />
+              <img
+                src="githubdm.png"
+                alt="github"
+                className="absolute top-0 left-0 w-full h-full object-cover hidden dark:block"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/ryan-acham-01b156237/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative w-16 h-16 rounded-lg overflow-hidden shadow-lg hover:scale-105 transform transition-all duration-300"
+            >
+              <img
+                src="linkedinlm.png"
+                alt="linkedin"
+                className="absolute top-0 left-0 w-full h-full object-cover dark:hidden"
+              />
+              <img
+                src="linkedindm.png"
+                alt="linkedin"
+                className="absolute top-0 left-0 w-full h-full object-cover hidden dark:block"
+              />
+            </a>
+          </div>
+        </div>
       </section>
     </div>
   );
